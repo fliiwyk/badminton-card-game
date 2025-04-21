2ace72354e67029ba743b715af794deb3c4930d5
Thom <thomas1298@hotmail.fr>
2025-04-21 18:21:32 +0200

    Serv function added

--- backend/src/game/game.ts
+++ backend/src/game/game.ts
@@ -5,8 +5,26 @@ import { Hand } from "../models/hand";
 
 console.log("=== DÉMARRAGE D’UN MATCH TEST ===");
 
-const Alice = new Player(1, "Alice", 0, new Hand(), true, 1);
-const Bob = new Player(2, "Bob", 0, new Hand(), false, 2);
-
-const match = new Match(1, "single", [Alice, Bob], 0, false);
-match.setupMatch(allCards);
+// Fonction simple pour tester ta logique directement :
+function runTest() {
+    const Alice = new Player(1, "Alice", 0, new Hand(), true, 1);
+    const Bob = new Player(2, "Bob", 0, new Hand(), false, 2);
+    const match = new Match(1, "single", [Alice, Bob], 0, false);
+  
+    match.setupMatch(allCards);
+    match.playServeCard();
+  
+    match.players.forEach((player) => {
+      console.log(`--- Player: ${player.getName()} ---`);
+      player.getHand().getCards().forEach((card) => {
+        match.isPlayableCard(card);
+        console.log({ card: card.toJSON(),});
+      });
+    });
+  
+    const middleDeckTopCard = match.middleDeck?.cards[0]?.toJSON();
+    console.log("Middle deck top card:", middleDeckTopCard);
+  }
+  
+  // Lancer directement le test
+  runTest();
